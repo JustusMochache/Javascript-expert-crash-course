a1d11169f9dc0de3aeb35144ad19a60b052d471a
Justus <justus.onyancha@moringaschool.com>
2023-01-02 13:29:01 +0300

    cover arrays and functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("testing js");
 console.log("Hello \n World");
 
+// strings
 
 let string = "abcd"
 
@@ -8,3 +9,47 @@ console.log(string.endsWith("d"));
 console.log(string.toLocaleUpperCase());
 console.log(string.substring(1));
 console.log(string.substring(1, 2));
+
+// objects using keys and values
+
+const person = {
+    name: "Diane Kemunto",
+    age: 24,
+    sign: "Libra",  
+};
+
+console.log(person);
+console.log(person.age);
+console.log(person.name);
+
+const map = new Map();
+map.set(456, "hello");
+map.set(123, "world");
+console.log(map.get(123));
+console.log(map.size);
+
+// arrays
+
+let array = [1,2,3,4,6];
+array.push(7);
+
+console.log(typeof array);
+
+// functions
+
+function addTwo(number = 5){
+     return number + 2;
+}
+
+console.log(addTwo(1));
+
+function callFunction(func, param){
+
+    console.log("+++++++++++++++++++++++++++++++++++++++++");
+    console.log(func(param))
+    ;
+
+}
+
+// callFunction(addTwo, 10);
+console.log(callFunction(addTwo, 10));
