f726ee535f27454f426f375fa4d8f5d272374378
Justus <justus.onyancha@moringaschool.com>
2023-01-02 12:57:00 +0300

    methods playground

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("testing js");
+console.log("Hello \n World");
+
+
+let string = "abcd"
+
+console.log(string.endsWith("d"));
+console.log(string.toLocaleUpperCase());
+console.log(string.substring(1));
+console.log(string.substring(1, 2));
